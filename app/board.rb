0ef9158d66ed3c1ad52e6a5c7fac0dbcb8b545fb
Paweł Gościcki <pawel.goscicki@gmail.com>
2014-09-03 21:02:12 +0200

    Introduce Board implementation with placing method

--- app/board.rb
+++ app/board.rb
@@ -1,3 +1,19 @@
 class Board
 
+  WIDTH = 5
+  HEIGHT = 5
+  DIRECTIONS = %w(NORTH EAST SOUTH WEST)
+
+  def place(position)
+    valid_position?(position)
+  end
+
+  private
+
+  def valid_position?(position)
+    position.x >= 0 && position.x <= WIDTH &&
+      position.y >= 0 && position.y <= HEIGHT &&
+      DIRECTIONS.include?(position.direction)
+  end
+
 end
--- spec/app/board_spec.rb
+++ spec/app/board_spec.rb
@@ -2,4 +2,58 @@ require 'spec_helper'
 
 describe Board do
 
+  describe 'HEIGHT/WIDTH class constants' do
+    specify 'are integers' do
+      expect(Board::HEIGHT).to be_an Integer
+      expect(Board::WIDTH).to be_an Integer
+    end
+
+    specify 'are greater than zero' do
+      expect(Board::HEIGHT).to be > 0
+      expect(Board::WIDTH).to be > 0
+    end
+  end
+
+  describe 'DIRECTIONS class constant' do
+    it 'is an Array with all possible directions' do
+      expect(Board::DIRECTIONS).to be_an Array
+    end
+  end
+
+  describe '#place' do
+    let(:board) { Board.new }
+
+    it 'returns truthy value if provided position is valid' do
+      position = Position[0, 0, 'SOUTH']
+
+      result = board.place(position)
+
+      expect(result).to be_truthy
+    end
+
+    it "returns falsey value if provided position exceeds board's width" do
+      position = Position[Board::WIDTH + 1, 0, 'SOUTH']
+
+      result = board.place(position)
+
+      expect(result).to be_falsey
+    end
+
+    it "returns falsey value if provided position exceeds board's height" do
+      position = Position[0, Board::HEIGHT + 1, 'SOUTH']
+
+      result = board.place(position)
+
+      expect(result).to be_falsey
+    end
+
+    it "returns falsey value if provided position's direction is invalid" do
+      position = Position[0, 0, 'gibberish']
+
+      result = board.place(position)
+
+      expect(result).to be_falsey
+    end
+  end
+
 end
